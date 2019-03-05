6741ce426fb01424e205a60916463444aacfaadb
Abdoulrazack <41553365+Abdoulrazack95@users.noreply.github.com>
2019-03-05 08:32:27 +0000

    express-workshop 2

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-const express = require("express");
-
+const express = require('express'); //load the server.
 const app = express();
+const handlebars = require('express-handlebars');// load handlebars.
+const blog = require('./data/blogPosts.json');
 
-app.use(express.static("public"));
+app.engine('handlebars', handlebars());//initialise handlebars
+app.set('view engine', 'handlebars')//specify engine for page rendering.
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  // res.sendFile(__dirname + '/views/index.html');
+  res.render('index', {
+    firstName: 'Abdoulrazack',
+    lastName: 'Ahmed', 
+    currentTime: new Date().toLocaleString(),
+    currentYearCopyRight: new Date().getFullYear(),
+    changeColor: 'style = "color: pink"',
+    pageName: 'Home',
+    blogPost: blog
+  });
 });
 
-app.get("/my-cv", (req, res) => {
-  res.sendFile(__dirname + "/views/my-cv.html");
+app.get('/my-cv', function(req, res){
+  res.render('my-cv', {
+    firstName: 'Abdoulrazack',
+    lastName: 'Ahmed',
+    pageName: 'My cv',
+    currentTime: new Date().toLocaleString()
+  });
 });
 
 const SERVER_PORT = process.env.PORT || 3000;
-app.listen(SERVER_PORT, function() {
-  console.log(
-    `Server is listening on port ${SERVER_PORT}. Ready to accept requests!`
-  );
-});
+app.listen(SERVER_PORT, function(){
+  console.log(`Server is listening on port 3000 ${SERVER_PORT}. Ready to accept requests!`);
+})
+
+
+
+
+
+
